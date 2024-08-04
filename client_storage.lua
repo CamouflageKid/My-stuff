1ac25f5ba255de50e0d626b347136f668abbc1e1
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-04 00:37:25 -0400

    Added file.

---
+++ client_storage.lua
@@ -0,0 +1,39 @@
+local history_table = {}
+
+
+function Main()
+    while true do
+        sleep(0.05)
+        if id then
+            term.setCursorPos(1, 1)
+            term.setCursorPos(1, 19)
+            local get = read(nil, history_table)
+
+            if history_table[#history_table] ~= get then
+                history_table[#history_table+1] = get
+            end
+
+            term.setCursorPos(1, 18)
+            term.clearLine()
+            term.setCursorPos(1, 1)
+
+            rednet.send(id, get, "is_in_storage")
+
+            local c, d = rednet.receive("is_in_storage.Return", 1)
+
+            if c then
+                print("There are "..d.." "..get.." in the system.")
+                term.setCursorPos(1, 15)
+                term.clearLine()
+                term.setCursorPos(1, 14)
+                term.clearLine()
+            end
+        end
+    end
+end
+
+
+require("client")
+
+Client_Init()
+Client_Loop(Main)
