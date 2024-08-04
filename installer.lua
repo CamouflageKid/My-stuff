e94d99682124c357e71025097ffab379b6ce6c11
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-04 00:29:27 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,19 +1,17 @@
-shell.setDir("disk/")
 shell.run("wget run https://raw.githubusercontent.com/SquidDev-CC/artist/HEAD/installer.lua")
 shell.run("wget https://raw.githubusercontent.com/CamouflageKid/My-stuff/main/item_data.lua")
 sleep(1)
-shell.run("move disk/item_data.lua disk/.artist.d/src/")
-fs.delete("disk/.artist.d/src/launch.lua")
+shell.run("move item_data.lua .artist.d/src/")
+fs.delete(".artist.d/src/launch.lua")
 
-shell.setDir("/.")
-local file = fs.open("disk/.artist.d/src/launch.lua", "w")
+local file = fs.open(".artist.d/src/launch.lua", "w")
 file.write('local context = require "artist"()\n-- Feel free to include custom modules here:\ncontext:require "item_data"\ncontext.config:save()\ncontext:run()')
 file.close()
 
 
 local file2 = fs.open("startup.lua", "w")
 if file2 ~= nil then
-  file2.write('shell.run("disk/artist.lua")')
+  file2.write('shell.run("artist.lua")')
 end
 file2.close()
 os.reboot()
