249aa55dc76ea629c2dbd42a2c88c3031d162512
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-03 23:25:08 -0400

    Added installer.

---
+++ installer.lua
@@ -0,0 +1,12 @@
+shell.run("wget run https://raw.githubusercontent.com/SquidDev-CC/artist/HEAD/installer.lua")
+shell.run("wget https://raw.githubusercontent.com/CamouflageKid/My-stuff/main/item_data.lua")
+fs.move("item_data.lua", ".artist.d/src/")
+fs.delete(".artist.d/src/launch.lua")
+local file = fs.open(".artist.d/src/launch.lua", "w")
+file.write('local context = require "artist"()\n-- Feel free to include custom modules here:\ncontext:require "item_data"\ncontext.config:save()\ncontext:run()')
+file.close()
+
+local file = fs.open("startup.lua", "w")
+file.write('shell.run("artist.lua")')
+file.close()
+os.reboot()
