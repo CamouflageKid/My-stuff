fb341e2244516b81dbac6bc090adf60c196d704c
That guy on the internet. <99547923+CamouflageKid@users.noreply.github.com>
2024-08-05 16:09:28 -0400

    Create craft_installer.lua

---
+++ craft_installer.lua
@@ -0,0 +1,5 @@
+shell.run("wget https://raw.githubusercontent.com/CamouflageKid/My-stuff/main/craft_storage.lua")
+local file = fs.open("startup.lua", "w")
+if file ~= nil then
+  file.write('shell.run("craft_storage.lua")')
+end
